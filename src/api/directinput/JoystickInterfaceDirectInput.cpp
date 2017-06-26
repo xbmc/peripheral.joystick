xbmc/peripheral.joystick

commit cd3e3f0dba9ef9de11949dd9ceda4634260114dc

Change to new C++ based addon interface style

src/api/directinput/JoystickInterfaceDirectInput.cpp
@@ -26,7 +26,7 @@
 #include "log/Log.h"
 #include "utils/CommonMacros.h"
 
-#include "libKODI_peripheral.h"
+#include <kodi/peripheral/libKODI_peripheral.h>
 
 #pragma comment(lib, "Dinput8.lib")
 #pragma comment(lib, "dxguid.lib")

commit 433812d56cce666d55997b91d52020275bf85739

Fix Windows compiler error introduced by cd3e3f0 in PR 112

src/api/directinput/JoystickInterfaceDirectInput.cpp
@@ -26,8 +26,6 @@
 #include "log/Log.h"
 #include "utils/CommonMacros.h"
 
-#include <kodi/peripheral/libKODI_peripheral.h>
-
 #pragma comment(lib, "Dinput8.lib")
 #pragma comment(lib, "dxguid.lib")
 

src/api/xinput/JoystickInterfaceXInput.cpp
@@ -34,12 +34,12 @@ using namespace JOYSTICK;
 
 ButtonMap CJoystickInterfaceXInput::m_buttonMap = {
     std::make_pair("game.controller.default", FeatureVector{
-        ADDON::JoystickFeature("leftmotor", JOYSTICK_FEATURE_TYPE_MOTOR),
-        ADDON::JoystickFeature("rightmotor", JOYSTICK_FEATURE_TYPE_MOTOR),
+        kodi::addon::JoystickFeature("leftmotor", JOYSTICK_FEATURE_TYPE_MOTOR),
+        kodi::addon::JoystickFeature("rightmotor", JOYSTICK_FEATURE_TYPE_MOTOR),
     }),
     std::make_pair("game.controller.ps", FeatureVector{
-        ADDON::JoystickFeature("strongmotor", JOYSTICK_FEATURE_TYPE_MOTOR),
-        ADDON::JoystickFeature("weakmotor", JOYSTICK_FEATURE_TYPE_MOTOR),
+        kodi::addon::JoystickFeature("strongmotor", JOYSTICK_FEATURE_TYPE_MOTOR),
+        kodi::addon::JoystickFeature("weakmotor", JOYSTICK_FEATURE_TYPE_MOTOR),
     }),
 };
 
@@ -91,12 +91,12 @@ bool CJoystickInterfaceXInput::ScanForJoysticks(JoystickVector& joysticks)
 const ButtonMap& CJoystickInterfaceXInput::GetButtonMap()
 {
   auto& dflt = m_buttonMap["game.controller.default"];
-  dflt[CJoystickXInput::MOTOR_LEFT].SetPrimitive(JOYSTICK_MOTOR_PRIMITIVE, ADDON::DriverPrimitive::CreateMotor(CJoystickXInput::MOTOR_LEFT));
-  dflt[CJoystickXInput::MOTOR_RIGHT].SetPrimitive(JOYSTICK_MOTOR_PRIMITIVE, ADDON::DriverPrimitive::CreateMotor(CJoystickXInput::MOTOR_RIGHT));
+  dflt[CJoystickXInput::MOTOR_LEFT].SetPrimitive(JOYSTICK_MOTOR_PRIMITIVE, kodi::addon::DriverPrimitive::CreateMotor(CJoystickXInput::MOTOR_LEFT));
+  dflt[CJoystickXInput::MOTOR_RIGHT].SetPrimitive(JOYSTICK_MOTOR_PRIMITIVE, kodi::addon::DriverPrimitive::CreateMotor(CJoystickXInput::MOTOR_RIGHT));
 
   auto& ps = m_buttonMap["game.controller.ps"];
-  ps[CJoystickXInput::MOTOR_LEFT].SetPrimitive(JOYSTICK_MOTOR_PRIMITIVE, ADDON::DriverPrimitive::CreateMotor(CJoystickXInput::MOTOR_LEFT));
-  ps[CJoystickXInput::MOTOR_RIGHT].SetPrimitive(JOYSTICK_MOTOR_PRIMITIVE, ADDON::DriverPrimitive::CreateMotor(CJoystickXInput::MOTOR_RIGHT));
+  ps[CJoystickXInput::MOTOR_LEFT].SetPrimitive(JOYSTICK_MOTOR_PRIMITIVE, kodi::addon::DriverPrimitive::CreateMotor(CJoystickXInput::MOTOR_LEFT));
+  ps[CJoystickXInput::MOTOR_RIGHT].SetPrimitive(JOYSTICK_MOTOR_PRIMITIVE, kodi::addon::DriverPrimitive::CreateMotor(CJoystickXInput::MOTOR_RIGHT));
 
   return m_buttonMap;
 }